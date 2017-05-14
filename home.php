b590fc7b59896f49fbb3cb3370350728a2122361
rey <reybatacjr@gmail.com>
2017-05-14 10:47:27 +0800

    Talk yangco

---
+++ home.php
@@ -0,0 +1,269 @@
+<?php
+get_header();
+?>
+<div class="container">
+	<div class="row">
+		<div id="qualified-programs-container" class="col-xs-12 col-md-8">
+			<div id="qualified-programs">
+				<div class="mask">
+					<h1>Qualified programs for English learners</h1>
+				<p style="max-width: 400px;">TALK Academy provides  variety of English programs for students who want to improve their English skills and prepare to take tests, apply for jobs and migrate to other countries.</p>
+				<div id="course-builder-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<strong>&nbsp;Course Builder:</strong>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>Speaking course</strong>
+								<p>ESL 521</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>Job application</strong>
+								<p>Intensive TOEIC</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>University &amp; emigration</strong>
+								<p>Intensive IELTS</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>Family Course</strong>
+								<p>Family ESL</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+		<div id="about-container" class="col-xs-12 col-sm-6 col-md-4">
+			<div id="about">
+				<div class="mask">
+					<h2>About TALK</h2>
+					<div id="about-normal">
+						<p>Preview TALK’S Classroom and Dormitory</p>
+						<span class="plus-sign">+</span>
+						<div class="clearfix"></div>
+					</div>
+					<div id="about-hover">
+						<p>
+						Before you start studying English in TALK Academy, You can check TALK’s classroom, dormitory and other facilities in 3D.<br>
+If you know the important information in advance,  it will be helpful for you  to study and stay in the Philippines
+						</p>
+						<span style="position: absolute;bottom: 10px;right: 10px;display: inline-block;margin-bottom: 0;" class="next-button"></span>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<div id="experience-container" class="col-xs-12 col-sm-6 col-md-4">
+			<div id="experience">
+				<div class="mask">
+					<h2>Student experience</h2>
+					<div id="experience-normal">
+						<p>Share your life in TALK Academy</p>
+						<span class="plus-sign">+</span>
+					</div>
+					<div id="experience-hover">
+						<p>
+							We recommend you to read TALK’s student’s articles which contain how they studied, stayed and traveled in the Philippines before you decide to study English in TALK Academy. They want to share their experiences with new students!
+						</p>
+						<span style="position: absolute;bottom: 10px;right: 10px;display: inline-block;margin-bottom: 0;" class="next-button"></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div id="notice-container" class="col-xs-12 col-md-4">
+			<div id="notice">
+				<strong>Notice</strong>
+				<p>
+					Before you study in Talk, you can check TALK’s news. It will be helpful for you to decide to study English in TALK Academy
+				</p>
+				<div id="notice-list-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<span id="notice-plus">+</span>
+							<span class="next-button"></span>
+						</div>
+					</div>
+					<ul id="notice-list">
+						<li><a href="#">5월 입학가능일 안내</a></li>
+						<li><a href="#">노용이 화난 날(Warning)</a></li>
+						<li><a href="#">노용이 소리 질른 날..ㅋㅋ</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="plans-container" class="col-xs-12 col-md-8">
+			<div id="plans">
+				<div class="mask">
+					<strong>Plans for learning English in the Philippines</strong>
+					<div id="plan-list-container" class="row">
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>IELTS<br>5.0, 5.5, 6.0</p>
+											<span class="plus-sign">+</span>
+										</div>	
+										<div class="plan-list-hover">
+											<p>Let’s prepare entering the university and migrating to other country with qualified TALK’s IELTS program together. </p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br><br>Power speaking</p>
+											<span class="plus-sign">+</span>
+										</div>	
+										<div class="plan-list-hover">
+											<p>Even though you are good at grammar and vocabulary, but your speaking is not good, we recommend that you take our power speaking course.</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>Short-term(1-3wks)<br>Study</p>
+											<span class="plus-sign">+</span>
+										</div>	
+										<div class="plan-list-hover">
+											<p>If you don’t have enough time to study English or don’t know how to study English, we will guide you to study English step by step..</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>TOEIC<br>600, 700, 800, 900</p>
+											<span class="plus-sign">+</span>
+										</div>
+										<div class="plan-list-hover">
+											<p>Let’s get your TOEIC Score and TOEIC Speaking efficiency with TALK Academy. The first school to start and operate TOEIC score guarantee program in the Philippines</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>Combination learning English in Philippines, Australia and Canada</p>
+											<span class="plus-sign">+</span>
+										</div>
+										<div class="plan-list-hover">
+											<p>If you feel, you need to study English more  before you start to study in university, why don’t you study English with us?</p>
+											<span class="next-button-black"></span>
+										</div>
+										
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>Affordable English learning made easy in TALK Academy</p>
+											<span class="plus-sign">+</span>
+										</div>
+										<div class="plan-list-hover">
+											<p>Do you feel tuition fee is expensive? We have budget-friendly English program for the learners. Just contact us.</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div id="best-place-container" class="col-xs-12 col-md-4">
+			<div id="best-place">
+				<div class="mask">
+					<strong>Best place for travelling</strong>
+					<div id="best-place-list-container">
+						<ul id="best-place-list">
+							<li><a href="#">Baguio City</a></li>
+							<li><a href="#">Day trip tour course</a></li>
+							<li><a href="#">Good for 1 or 2 nights tour course</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="campaign-container" class="col-xs-12 col-md-4">
+			<div id="campaign">
+				<strong>Campaign</strong>
+				<div id="campaign-list-container">
+					<ul id="campaign-list">
+						<li><a href="#">Prepare for IELTS test with Native teacher</a></li>
+						<li><a href="#">Get more than 300 TOEIC score in 12weeks</a></li>
+						<li><a href="#">Let’s get official TOEIC test for free in TALK</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="calendar-container" class="col-xs-12 col-md-4">
+			<div id="calendar">
+				<strong>Calendar</strong>
+				<div id="calendar-list-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="calendar-list-parent col-xs-6" style="border-right:1px solid #666; border-bottom: 1px solid #666">
+								<div class="calendar-list">
+									<p style="padding-top: 15px;">Holiday<br>in Philippines</p>
+								</div>
+							</div>
+							<div class="calendar-list-parent col-xs-6" style=" border-bottom: 1px solid #666">
+								<div class="calendar-list">
+									<p>Official<br>pick-up<br>Service dates</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="calendar-list-parent col-xs-6" style="border-right:1px solid #666">
+								<div class="calendar-list">
+									<p>IELTS<br>Official test<br>dates</p>
+								</div>
+							</div>
+							<div class="calendar-list-parent col-xs-6">
+								<div class="calendar-list">
+									<p>TOEIC<br>Official test<br>dates</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<img src="<?php echo THEME_URI; ?>/img/sample-image1.png" class="img-responsive"/>
+</div>
+<?php 
+get_footer();
