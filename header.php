72550794f5cb626fe7acc8bd616122cf107caecd
reynaldob <reynaldob@sonarlogic.biz>
2017-11-04 11:02:36 +0800

    update font reference

--- header.php
+++ header.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php bloginfo('name'); ?> <?php wp_title('&laquo;', true, 'right'); ?></title>
     <meta name="description" content="<?php bloginfo('description'); ?>" />
+
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine">
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -334,4 +336,3 @@
                   </nav>
               </div>
       </header>
-
