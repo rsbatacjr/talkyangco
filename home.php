e46760767a4bf3a2195234bf079f8b15dbd13d71
root <root@ubuntu-2gb-sgp1-01.localdomain>
2018-03-01 08:28:20 +0000

    modified home.php

--- home.php
+++ home.php
@@ -276,6 +276,7 @@ If you know the important information in advance,  it will be helpful for you  t
 	<div class="col-xs-12">
 		<?php do_shortcode("[show_footerImages post_type='inside-philippines']"); ?>
 	</div>
+	<!-- <img src="<?php echo THEME_URI; ?>/img/sample-image1.png" class="img-responsive"/> -->
 </div>
 <div class="row">
 	<div class="col-xs-12 clearfix">&nbsp;</div>
